c82375f37873480074ca6649254c77033ef69970
Rob Dooh <rob.dooh@threestonestudios.com>
2015-11-15 01:28:55 -0500

    add ecmascript

--- Meteor_Seed_Project/.meteor/local/build/programs/web.browser/app/client/views/layouts/blank.js
+++ Meteor_Seed_Project/.meteor/local/build/programs/web.browser/app/client/views/layouts/blank.js
@@ -1,13 +1,22 @@
-(function(){Template.blankLayout.rendered = function(){
+(function(){
 
-    // Add gray color for background in blank layout
-    $('body').addClass('gray-bg');
+/////////////////////////////////////////////////////////////////////////
+//                                                                     //
+// client/views/layouts/blank.js                                       //
+//                                                                     //
+/////////////////////////////////////////////////////////////////////////
+                                                                       //
+Template.blankLayout.rendered = function () {                          // 1
+                                                                       //
+    // Add gray color for background in blank layout                   //
+    $('body').addClass('gray-bg');                                     // 4
+};                                                                     //
+                                                                       //
+Template.blankLayout.destroyed = function () {                         // 8
+                                                                       //
+    // Remove special color for blank layout                           //
+    $('body').removeClass('gray-bg');                                  // 11
+};                                                                     //
+/////////////////////////////////////////////////////////////////////////
 
-}
-
-Template.blankLayout.destroyed = function(){
-
-    // Remove special color for blank layout
-    $('body').removeClass('gray-bg');
-};
 }).call(this);
